450b1cd7a052083c2c4791b2760499de8b53a8ca
D-Aldin <dobricaldin@gmail.com>
2025-06-01 21:25:47 +0200

    restructure the review section from the portfolio.component.html into a component at its own

--- portfolio/src/app/main-content/portfolio/portfolio.component.ts
+++ portfolio/src/app/main-content/portfolio/portfolio.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProjectsComponent } from './projects/projects.component';
 import { PROJECTS } from './projects/projects';
+import { ReviewComponent } from './review/review.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [ProjectsComponent],
+  imports: [ProjectsComponent, ReviewComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
 })
