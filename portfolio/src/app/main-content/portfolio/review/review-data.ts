8cb4e4cecd9e154563577e1c8758ea92c86ac773
D-Aldin <dobricaldin@gmail.com>
2025-06-02 22:08:01 +0200

    place the exclamation mark icon over the review box

--- portfolio/src/app/main-content/portfolio/review/review-data.ts
+++ portfolio/src/app/main-content/portfolio/review/review-data.ts
@@ -1,9 +1,9 @@
 export const REVIEW_DATA = [
   {
-    name: 'Robby',
+    name: 'Robby Runge',
     review:
       'Aldin war ein äußerst fleißiger und disziplinierter Mitarbeiter, der seine Aufgaben stets schnell und präzise erledigt hat. Seine Arbeitsmoral war vorbildlich – egal wie herausfordernd das Projekt war, man konnte sich immer auf ihn verlassen. Dabei war er nicht nur ein zuverlässiger Einzelkämpfer, sondern auch ein toller Teamplayer, der stets unterstützend und kooperativ mit uns zusammengearbeitet hat. Seine positive Einstellung und sein Engagement haben das Team spürbar gestärkt.',
-    image: 'https://randomuser.me/api/portraits',
+    image: './../../../../assets/icons/portfolio/review/robby.png',
   },
 
   {
