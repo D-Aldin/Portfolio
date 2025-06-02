0bd9845c7a92c69b54d5ef0b4282104fd56e1888
D-Aldin <dobricaldin@gmail.com>
2025-06-02 19:56:18 +0200

    add the logic and style the points for the review section

---
+++ portfolio/src/app/main-content/portfolio/review/review-data.ts
@@ -0,0 +1,20 @@
+export const REVIEW_DATA = [
+  {
+    name: 'Robby',
+    review:
+      'Aldin war ein äußerst fleißiger und disziplinierter Mitarbeiter, der seine Aufgaben stets schnell und präzise erledigt hat. Seine Arbeitsmoral war vorbildlich – egal wie herausfordernd das Projekt war, man konnte sich immer auf ihn verlassen. Dabei war er nicht nur ein zuverlässiger Einzelkämpfer, sondern auch ein toller Teamplayer, der stets unterstützend und kooperativ mit uns zusammengearbeitet hat. Seine positive Einstellung und sein Engagement haben das Team spürbar gestärkt.',
+    image: 'https://randomuser.me/api/portraits',
+  },
+
+  {
+    name: 'Wladi Bender',
+    review: 'test data',
+    image: 'https://randomuser.me/api/portraits',
+  },
+
+  {
+    name: 'Semiur ',
+    review: 'new test',
+    image: 'https://randomuser.me/api/portraits',
+  },
+];
--- portfolio/src/app/main-content/portfolio/review/review.component.ts
+++ portfolio/src/app/main-content/portfolio/review/review.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { REVIEW_DATA } from './review-data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-review',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './review.component.html',
-  styleUrl: './review.component.scss'
+  styleUrl: './review.component.scss',
 })
 export class ReviewComponent {
+  reviews = REVIEW_DATA;
+  review: string = '';
+  name: string = '';
+  counter: number = 0;
+  selectedReview: number = 0;
 
+  nextReview(): void {
+    this.counter++;
+    if (this.counter >= this.reviews.length) {
+      this.counter = 0;
+    }
+    this.selectedReview = this.counter;
+  }
+
+  backReview(): void {
+    this.selectedReview = this.counter;
+    if (this.counter <= 0) {
+      this.counter = this.reviews.length - 1;
+    } else {
+      this.counter--;
+    }
+  }
 }
