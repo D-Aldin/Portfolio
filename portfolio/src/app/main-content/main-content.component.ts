ea665924fd31f3d949ed11ffde02d21b6966cbbc
D-Aldin <dobricaldin@gmail.com>
2025-06-03 22:19:29 +0200

    generate contact-form component and style it

--- portfolio/src/app/main-content/main-content.component.ts
+++ portfolio/src/app/main-content/main-content.component.ts
@@ -4,6 +4,7 @@ import { LandingPageComponent } from './landingPage/landingPage.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
@@ -14,6 +15,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
     AboutMeComponent,
     MySkillsComponent,
     PortfolioComponent,
+    ContactComponent,
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
