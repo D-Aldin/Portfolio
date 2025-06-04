068e470b1a27ffc13917ce54da1e3d9dd73596cb
D-Aldin <dobricaldin@gmail.com>
2025-06-04 23:47:35 +0200

    style the checkbox in contact-form

--- portfolio/src/app/main-content/contact/contact-form/contact-form.component.ts
+++ portfolio/src/app/main-content/contact/contact-form/contact-form.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrl: './contact-form.component.scss',
 })
 export class ContactFormComponent {
+  http = inject(HttpClient);
 
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://www.aldin-dobric.de//sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
+    }
+  }
 }
