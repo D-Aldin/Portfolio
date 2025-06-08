bbc2bd488b0aad77c27414606ac5a5936d328820
D-Aldin <dobricaldin@gmail.com>
2025-06-08 18:46:50 +0200

    finish the translation in the about me component

--- portfolio/src/app/main-content/about-me/about-me.component.ts
+++ portfolio/src/app/main-content/about-me/about-me.component.ts
@@ -21,24 +21,17 @@ export class AboutMeComponent {
     {
       icon: '../../../assets/icons/about/Capa_small.png',
       alt: 'location',
-      text: `I’m currently living in Frankfurt am Main, a city I really enjoy—but 
-             I’m also very open when it comes to how and where I work. Whether it’s working remotely, 
-             collaborating in a hybrid setup, or even relocating for the right opportunity, I’m flexible and excited about new experiences. 
-             What matters most to me is being part of a team where I can grow, contribute, and build meaningful projects.`,
+
       cssClass: 'location',
     },
     {
       icon: '../../../assets/icons/about/Vector_small.png',
       alt: 'light-bulb',
-      text: `I’m always curious and open to learning something new. Whether it’s a new tech, tool, or way of working, 
-             I enjoy challenging myself and picking up new skills. I love the feeling of growth and seeing how each step improves my work.`,
       cssClass: 'improving',
     },
     {
       icon: '../../../assets/icons/about/puzzle_small.png',
       alt: 'puzzle',
-      text: `When I face a problem, I like to mix analytical thinking with creativity to find the best solution. I enjoy breaking things down and exploring different ideas, and 
-      I’m always open to collaborating with others to get fresh perspectives. Every challenge teaches me something new, and I use that to keep growing and getting better.`,
       cssClass: 'problem-solving',
     },
   ];
--- portfolio/src/app/translation.ts
+++ portfolio/src/app/translation.ts
@@ -9,35 +9,42 @@ export const TRANSLATION = {
       developer: 'FRONTEND DEVELOPER',
     },
 
-    aboueMe: {
+    aboutMe: {
       title: 'About me',
-      description:
-        'My journey into programming began with curiosity, especially when I discovered Python.\n\
-         I was fascinated by how a simple and readable language could be used to automate tasks and build logical systems.\n\
-         After building a strong foundation in Python, I moved on to web development and began learning HTML, CSS, and JavaScript.\n\
-         I was drawn to the visual and interactive side of coding—being able to create things users can see and use in real time was incredibly rewarding.\n\
-         Over time, coding grew from an interest into a true passion. I love the creativity, problem-solving, and constant learning it offers.',
+      description: `
+        My journey into programming began with curiosity, especially when I discovered Python.
+        I was fascinated by how a simple and readable language could be used to automate tasks and build logical systems.
+        After building a strong foundation in Python, I moved on to web development and began learning HTML, CSS, and JavaScript.
+        I was drawn to the visual and interactive side of coding—being able to create things users can see and use in real time was incredibly rewarding.
+        Over time, coding grew from an interest into a true passion. I love the creativity, problem-solving, and constant learning it offers.
+      `,
+      blocks: [
+        `I’m currently living in Frankfurt am Main, a city I really enjoy—but I’m also very open when it comes to how and where I work. Whether it’s working remotely, collaborating in a hybrid setup, or even relocating for the right opportunity, I’m flexible and excited about new experiences. What matters most to me is being part of a team where I can grow, contribute, and build meaningful projects.`,
+        `I’m always curious and open to learning something new. Whether it’s a new tech, tool, or way of working, I enjoy challenging myself and picking up new skills. I love the feeling of growth and seeing how each step improves my work.`,
+        `When I face a problem, I like to mix analytical thinking with creativity to find the best solution. I enjoy breaking things down and exploring different ideas, and I’m always open to collaborating with others to get fresh perspectives. Every challenge teaches me something new, and I use that to keep growing and getting better.`,
+      ],
     },
 
     skills: {
       title: 'My skills',
-      descriptionExperience:
-        "I've built several front-end projects using different tools. In my Join Kanban app, I worked with HTML, CSS, JavaScript, and Firebase.\n\
-            I created a Valkyrie game using OOP in JavaScript, and built Pokémon Desk with a REST API.\n\
-            I also enjoy working with TypeScript, Angular, Material Design, and using Git in Scrum-based teams.",
-
+      descriptionExperience: `
+        I've built several front-end projects using different tools. In my Join Kanban app, I worked with HTML, CSS, JavaScript, and Firebase.
+        I created a Valkyrie game using OOP in JavaScript, and built Pokémon Desk with a REST API.
+        I also enjoy working with TypeScript, Angular, Material Design, and using Git in Scrum-based teams.
+      `,
       lookingFor: 'Looking for',
       anotherSkill: 'another skill',
-      descriptionAnother:
-        "I'm always excited to learn new technologies and frameworks.\n\
-            I enjoy exploring how different tools work together to build better, faster, and more dynamic applications.\n\
-            Whether it's diving into a new JavaScript framework, experimenting with backend services like Firebase, or refining UI with modern design systems,\n\
-            I love the challenge of staying up to date and constantly improving my skills.",
+      descriptionAnother: `
+        I'm always excited to learn new technologies and frameworks.
+        I enjoy exploring how different tools work together to build better, faster, and more dynamic applications.
+        Whether it's diving into a new JavaScript framework, experimenting with backend services like Firebase, or refining UI with modern design systems,
+        I love the challenge of staying up to date and constantly improving my skills.
+      `,
     },
 
     portfolio: {
       guide:
-        'Explore a selection of my work here- Interact with projects to see my skills in action',
+        'Explore a selection of my work here – Interact with projects to see my skills in action',
     },
   },
 
@@ -51,30 +58,37 @@ export const TRANSLATION = {
       developer: 'FRONTEND-ENTWICKLER',
     },
 
-    aboueMe: {
+    aboutMe: {
       title: 'Über mich',
-      description:
-        'Meine Reise in die Programmierung begann aus Neugier, besonders als ich Python entdeckte.\n\
-         Ich war fasziniert davon, wie eine einfache und gut lesbare Sprache genutzt werden kann, um Aufgaben zu automatisieren und logische Systeme zu entwickeln.\n\
-         Nachdem ich ein solides Fundament in Python aufgebaut hatte, wechselte ich zur Webentwicklung und begann mit HTML, CSS und JavaScript.\n\
-         Mich faszinierte die visuelle und interaktive Seite des Programmierens – Dinge zu erschaffen, die Nutzer in Echtzeit sehen und verwenden können, war unglaublich befriedigend.\n\
-         Mit der Zeit wurde Programmieren von einem Interesse zu einer echten Leidenschaft. Ich liebe die Kreativität, das Lösen von Problemen und das ständige Lernen.',
+      description: `
+        Meine Reise in die Programmierung begann aus Neugier, besonders als ich Python entdeckte.
+        Ich war fasziniert davon, wie eine einfache und gut lesbare Sprache genutzt werden kann, um Aufgaben zu automatisieren und logische Systeme zu entwickeln.
+        Nachdem ich ein solides Fundament in Python aufgebaut hatte, wechselte ich zur Webentwicklung und begann mit HTML, CSS und JavaScript.
+        Mich faszinierte die visuelle und interaktive Seite des Programmierens – Dinge zu erschaffen, die Nutzer in Echtzeit sehen und verwenden können, war unglaublich befriedigend.
+        Mit der Zeit wurde Programmieren von einem Interesse zu einer echten Leidenschaft. Ich liebe die Kreativität, das Lösen von Problemen und das ständige Lernen.
+      `,
+      blocks: [
+        `Ich lebe derzeit in Frankfurt am Main – einer Stadt, die mir sehr gefällt. Ich bin aber sehr offen, was meine Arbeitsweise und meinen Arbeitsort angeht. Ob Remote-Arbeit, hybride Zusammenarbeit oder ein Umzug für die richtige Gelegenheit – ich bin flexibel und freue mich auf neue Erfahrungen. Am wichtigsten ist mir, Teil eines Teams zu sein, in dem ich wachsen, beitragen und sinnvolle Projekte umsetzen kann.`,
+        `Ich bin immer neugierig und offen, etwas Neues zu lernen – ob eine neue Technologie, ein Tool oder eine andere Arbeitsweise. Ich liebe es, mich selbst herauszufordern und neue Fähigkeiten zu erlernen. Das Gefühl, Fortschritte zu machen und sich ständig zu verbessern, motiviert mich sehr.`,
+        `Wenn ich auf ein Problem stoße, versuche ich, analytisches Denken mit Kreativität zu kombinieren, um die beste Lösung zu finden. Ich breche Probleme gerne in Einzelschritte auf, erkunde verschiedene Ideen und arbeite gerne mit anderen zusammen, um neue Perspektiven zu erhalten. Jede Herausforderung bringt mir etwas Neues bei und hilft mir, weiter zu wachsen.`,
+      ],
     },
 
     skills: {
       title: 'Meine Fähigkeiten',
-      descriptionExperience:
-        'Ich habe mehrere Frontend-Projekte mit verschiedenen Tools erstellt. In meiner Join Kanban-App arbeitete ich mit HTML, CSS, JavaScript und Firebase.\n\
-            Ich entwickelte ein Walküren-Spiel mit OOP in JavaScript und baute Pokémon Desk mit einer REST-API.\n\
-            Außerdem arbeite ich gerne mit TypeScript, Angular, Material Design und nutze Git in Scrum-basierten Teams.',
-
+      descriptionExperience: `
+        Ich habe mehrere Frontend-Projekte mit verschiedenen Tools erstellt. In meiner Join Kanban-App arbeitete ich mit HTML, CSS, JavaScript und Firebase.
+        Ich entwickelte ein Walküren-Spiel mit OOP in JavaScript und baute Pokémon Desk mit einer REST-API.
+        Außerdem arbeite ich gerne mit TypeScript, Angular, Material Design und nutze Git in Scrum-basierten Teams.
+      `,
       lookingFor: 'Auf der Suche nach',
       anotherSkill: 'weiterer Fähigkeit',
-      descriptionAnother:
-        'Ich bin immer begeistert, neue Technologien und Frameworks zu lernen.\n\
-            Ich erforsche gerne, wie verschiedene Tools zusammenwirken, um bessere, schnellere und dynamischere Anwendungen zu entwickeln.\n\
-            Ob beim Eintauchen in ein neues JavaScript-Framework, beim Experimentieren mit Backend-Services wie Firebase oder beim Verfeinern der UI mit modernen Designsystemen –\n\
-            ich liebe die Herausforderung, auf dem neuesten Stand zu bleiben und meine Fähigkeiten ständig zu verbessern.',
+      descriptionAnother: `
+        Ich bin immer begeistert, neue Technologien und Frameworks zu lernen.
+        Ich erforsche gerne, wie verschiedene Tools zusammenwirken, um bessere, schnellere und dynamischere Anwendungen zu entwickeln.
+        Ob beim Eintauchen in ein neues JavaScript-Framework, beim Experimentieren mit Backend-Services wie Firebase oder beim Verfeinern der UI mit modernen Designsystemen –
+        ich liebe die Herausforderung, auf dem neuesten Stand zu bleiben und meine Fähigkeiten ständig zu verbessern.
+      `,
     },
 
     portfolio: {
