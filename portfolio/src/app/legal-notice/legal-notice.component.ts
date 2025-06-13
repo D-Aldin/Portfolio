fea76ccb23ada00a0077d88c7d48c447dab2f973
D-Aldin <dobricaldin@gmail.com>
2025-06-14 00:17:28 +0200

    add impressum translation

--- portfolio/src/app/legal-notice/legal-notice.component.ts
+++ portfolio/src/app/legal-notice/legal-notice.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../main-content/header/header.component';
+import { LanguageService } from './../../app/language';
+import { TRANSLATION } from './../../app/translation';
 
 @Component({
   selector: 'app-legal-notice',
@@ -8,4 +10,12 @@ import { HeaderComponent } from '../main-content/header/header.component';
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss',
 })
-export class LegalNoticeComponent {}
+export class LegalNoticeComponent {
+  constructor(private languageService: LanguageService) {}
+
+  translation = TRANSLATION.en.legalNotice;
+
+  get langPath() {
+    return this.languageService.getCurrentTranslations();
+  }
+}
--- portfolio/src/app/translation.ts
+++ portfolio/src/app/translation.ts
@@ -56,6 +56,47 @@ export const TRANSLATION = {
                 Let’s build something exceptional together!",
       contactMe: 'Need a Frontend developer? Contact me!',
     },
+
+    legalNotice: {
+      liability: ['Disclaimer'],
+      liabilityForContent: [
+        'Liability for Content',
+        'The contents of our pages were created with the utmost care. However, we cannot guarantee the accuracy, completeness, or timeliness of the content.\n\
+         As a service provider, we are responsible for our own content on these pages in accordance with § 7 (1) DDG and general laws.\n\
+         According to §§ 8 to 10 DDG, we are not obligated as a service provider to monitor transmitted or stored external information\n\
+         or to investigate circumstances that indicate illegal activity. Obligations to remove or block the use of information under general laws remain unaffected.\n\
+         However, liability in this regard is only possible from the moment of knowledge of a specific legal violation.\n\
+         Upon becoming aware of such legal violations, we will remove this content immediately.',
+      ],
+      liabilityForLinks: [
+        'Liability for Links',
+        'Our website contains links to external websites of third parties, over whose content we have no control.\n\
+         Therefore, we cannot accept any responsibility for this external content. The respective provider or operator of the linked sites is always responsible for their content.\n\
+         The linked sites were checked for possible legal violations at the time of linking. Illegal content was not recognizable at the time of linking.\n\
+         A permanent content control of the linked pages is not reasonable without concrete indications of a legal violation.\n\
+         If we become aware of any legal infringements, we will remove such links immediately.',
+      ],
+      copyright: [
+        'Copyright',
+        'The content and works created by the site operators on these pages are subject to German copyright law.\n\
+         Duplication, processing, distribution, and any kind of use outside the limits of copyright law require the written consent\n\
+         of the respective author or creator. Downloads and copies of this site are only permitted for private, non-commercial use.\n\
+         If the content on this site was not created by the operator, the copyrights of third parties are respected.\n\
+         In particular, third-party content is marked as such. If you still notice a copyright infringement, please inform us accordingly.\n\
+         Upon becoming aware of legal violations, we will remove such content immediately.',
+      ],
+      dataProtection: [
+        'Data Protection',
+        `The use of our website is generally possible without providing personal data. Insofar as personal data
+         (such as name, address, or email addresses) is collected on our site, this is always done on a voluntary basis, if possible.
+         This data will not be passed on to third parties without your express consent.
+         We point out that data transmission over the Internet (e.g., communication via email) can have security vulnerabilities.
+         Complete protection of data from access by third parties is not possible.
+         The use of contact data published within the scope of the imprint obligation by third parties to send unsolicited advertising and
+         information materials is hereby expressly prohibited. The operators of the site expressly reserve the right to take legal action in the event of
+         unsolicited advertising being sent, such as through spam emails.`,
+      ],
+    },
   },
 
   // GERMAN ---
@@ -112,12 +153,55 @@ export const TRANSLATION = {
       title: 'Ein Problem zu lösen?',
       content:
         'Wenn du auf der Suche nach einem leidenschaftlichen und zuverlässigen Frontend-Entwickler bist, der deine Ideen zum Leben erweckt, dann melde dich gerne bei mir! Ich bin offen für neue Chancen, spannende Kooperationen oder einfach ein gutes Gespräch über großartige digitale Erlebnisse.<br><br>\
-    Lass uns vernetzen und gemeinsam etwas Bedeutungsvolles schaffen!\
-    Ich bin ein Frontend-Entwickler mit einem starken Fokus auf benutzerfreundliche, responsive und visuell ansprechende Web-Oberflächen. Besonders interessiere ich mich für Rollen, in denen ich eng mit Design- und Backend-Teams zusammenarbeiten kann, um nahtlose Nutzererfahrungen zu gestalten.<br><br>\
-    Ich bringe echten Mehrwert für dein Projekt, indem ich sauberen, wartbaren Code mit einem tiefen Verständnis für UI/UX-Prinzipien verbinde. Dabei lege ich großen Wert auf Performance, Barrierefreiheit und ein intuitives Nutzungserlebnis. Ob es darum geht, Ladezeiten zu optimieren, die mobile Darstellung zu verbessern oder komplexe Features in reibungslose Interaktionen zu verwandeln – ich sorge dafür, dass deine Vision Wirklichkeit wird.<br><br>\
-    Lass uns gemeinsam etwas Außergewöhnliches erschaffen!',
+         Lass uns vernetzen und gemeinsam etwas Bedeutungsvolles schaffen!\
+         Ich bin ein Frontend-Entwickler mit einem starken Fokus auf benutzerfreundliche, responsive und visuell ansprechende Web-Oberflächen. Besonders interessiere ich mich für Rollen, in denen ich eng mit Design- und Backend-Teams zusammenarbeiten kann, um nahtlose Nutzererfahrungen zu gestalten.<br><br>\
+         Ich bringe echten Mehrwert für dein Projekt, indem ich sauberen, wartbaren Code mit einem tiefen Verständnis für UI/UX-Prinzipien verbinde. Dabei lege ich großen Wert auf Performance, Barrierefreiheit und ein intuitives Nutzungserlebnis. Ob es darum geht, Ladezeiten zu optimieren, die mobile Darstellung zu verbessern oder komplexe Features in reibungslose Interaktionen zu verwandeln – ich sorge dafür, dass deine Vision Wirklichkeit wird.<br><br>\
+         Lass uns gemeinsam etwas Außergewöhnliches erschaffen!',
       contactMe:
         'Brauchen Sie einen Frontend-Entwickler? Kontaktieren Sie mich!',
+    },
+
+    legalNotice: {
+      liability: ['Haftungsausschluss'],
+      liabilityForContent: [
+        'Haftung für Inhalte',
+        'Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen.\n\
+         Als Diensteanbieter sind wir gemäß § 7 Abs.1 DDG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich.\n\
+         Nach §§ 8 bis 10 DDG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach\n\
+         Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den\n\
+         allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich.\n\
+         Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.',
+      ],
+      liabilityForLinks: [
+        'Haftung für Links',
+        'Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben.\n\
+         Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder\n\
+         Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.\n\
+         Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist\n\
+         jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.',
+      ],
+
+      copyright: [
+        'Urheberrecht',
+        'Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht.\n\
+         Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der\n\
+         schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.\n\
+         Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche\n\
+         gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von\n\
+         Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.',
+      ],
+
+      dataProtection: [
+        'Datenschutz',
+        `Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten
+         (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis.
+         Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.
+         Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann.
+         Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.
+         Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und
+         Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten
+         Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.`,
+      ],
     },
   },
 };
