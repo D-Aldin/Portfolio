7b63fc2674bd73962a563e4be7140eb4b17b1853
D-Aldin <dobricaldin@gmail.com>
2025-06-13 21:14:37 +0200

    add impressum

--- portfolio/src/app/legal-notice/legal-notice.component.ts
+++ portfolio/src/app/legal-notice/legal-notice.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../main-content/header/header.component';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './legal-notice.component.html',
-  styleUrl: './legal-notice.component.scss'
+  styleUrl: './legal-notice.component.scss',
 })
-export class LegalNoticeComponent {
-
-}
+export class LegalNoticeComponent {}
--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -1,7 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
+import { withInMemoryScrolling } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(withFetch())],
-});
+  providers: [
+    provideHttpClient(withFetch()),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+    ),
+  ],
+}).catch((err) => console.error(err));
