00f9d9e66d1c8efc4915bfc5b94edbacf440e1dc
D-Aldin <dobricaldin@gmail.com>
2025-06-17 00:33:09 +0200

    add some style and logic to the burger menu

--- portfolio/src/app/main-content/header/burger-menu/burger-menu.component.ts
+++ portfolio/src/app/main-content/header/burger-menu/burger-menu.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Renderer2, OnDestroy } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
+
 @Component({
   selector: 'app-burger-menu',
   standalone: true,
@@ -9,9 +12,26 @@ import { CommonModule } from '@angular/common';
   styleUrl: './burger-menu.component.scss',
 })
 export class BurgerMenuComponent {
+  constructor(
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
   isOpen: boolean = false;
 
   toogleMenuBurger() {
     this.isOpen = !this.isOpen;
+
+    if (this.isOpen) {
+      this.renderer.addClass(document.body, 'no-scroll');
+    } else {
+      this.renderer.removeClass(document.body, 'no-scroll');
+    }
+  }
+
+  ngOnDestroy() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.renderer.removeClass(document.body, 'no-scroll');
+    }
   }
 }
